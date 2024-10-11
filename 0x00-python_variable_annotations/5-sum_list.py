da143f0fe999ad38bb0775b4f9d66b2efc4d5f4e
samlan24 <muchemionline@gmial.com>
2024-10-11 16:24:27 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""returns their sum as a float"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """returns sum of all floats in the list"""
+    return sum(input_list)
