b53c0a9971e4bacfb905bb92263e49c7992e0979
samlan24 <muchemionline@gmial.com>
2024-10-14 11:32:22 +0000

    0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""asyncio module"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int) -> int:
+    """delays for a max delay_max seconds and then returns the delay"""
+    delay = random.randint(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
