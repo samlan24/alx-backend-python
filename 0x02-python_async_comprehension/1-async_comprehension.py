daaaacb56bb53429383f15b11138718a4ec39605
samlan24 <muchemionline@gmial.com>
2024-10-15 13:23:06 +0000

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-"""collect 10 random numbers using an async comprehensing over async_generator,
-then return the 10 random numbers."""
+"""Import async_generator from the previous task and then write a coroutine
+called async_comprehension that takes no arguments.
+
+The coroutine will collect 10 random numbers using an async comprehensing
+over async_generator, then return the 10 random numbers.
+"""
 
 
 from typing import List
@@ -8,7 +12,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """collect 10 random numbers using an async comprehensing over async_generator,
-    then return the 10 random numbers."""
-    results = [n async for n in async_generator()]
+    """Return the 10 random numbers"""
+    results = [i async for i in async_generator()]
     return results
